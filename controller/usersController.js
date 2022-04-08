b540d046c50b30d87a80176f2f4b2df761b752d6
Shuvo Haldar <86658217+shuvo-h@users.noreply.github.com>
2022-04-08 13:26:33 +0600

    new user insert to DB through mongoose

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,4 +1,28 @@
-async function addUser(req, res, next) {}
+// external imports
+const bcrypt = require("bcrypt");
+
+// internal imports
+const User = require("../models/People");
+
+async function addUser(req, res, next) {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    newUser = new User({
+        ...req.body,
+        password: hashedPassword,
+    });
+
+     // save user or send error
+    try {
+        const result = await newUser.save();
+        res.status(200).json({ message: "User was added successfully!" });
+    } catch (err) {
+        res.status(500).json({
+        errors: {
+            common: { msg: "Unknown error occured!" },
+        },
+        });
+    }
+}
 
 module.exports = {
     addUser,
